2d3a46a2678b0831f7c77ba5cf3c57f54068c54f
Peter Januarius <pete@nexgencodecamp.com.au>
2018-04-29 09:31:59 +1000

    init and level

---
+++ js/level-maps.js
@@ -0,0 +1,32 @@
+/**
+ * This is where all the levels are placed. To add another one, simply create
+ * a const called LEVEL_XXX where XXX is the number of the level eg. TWO, THREE...etc
+ */
+const LEVEL_ONE = [
+    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+    "W                                          W",
+    "W                                          W",
+    "W                                          W",
+    "W                    PPPPPPPPPPP           W",
+    "W                                          W",
+    "W                                          W",
+    "W                                          W",
+    "W    PPPPPPPP                              W",
+    "W                                          W",
+    "W                          PPPPPPP         W",
+    "W                 PPPPPP                   W",
+    "W                                          W",
+    "W         PPPPPPP                          W",
+    "W                                          W",
+    "W                     PPPPPP               W",
+    "W                                          W",
+    "W   PPPPPPPPPPP                            W",
+    "W                                          W",
+    "W                 PPPPPPPPPPP              W",
+    "W                                          W",
+    "W                                          W",
+    "W                                          W",
+    "W                                          W",
+    "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",   
+];
+
